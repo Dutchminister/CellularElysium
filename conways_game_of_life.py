411b3c3719ef8057915e50b6e759735a54617c8a
Erik Coleman <erik.coleman@nidec-mce.com>
2023-12-21 09:48:27 -0800

    Changed colors

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -10,7 +10,7 @@ class GameOfLife:
 
         pygame.init()
         self.screen = pygame.display.set_mode((width, height))
-        pygame.display.set_caption("Conway's Game of Life - Toroidal Grid")
+        pygame.display.set_caption("Conway's Game of Life - A Cosmic Dance of Cells")
 
         self.grid = self.initialize_grid()
         self.clock = pygame.time.Clock()
@@ -18,18 +18,18 @@ class GameOfLife:
     def initialize_grid(self):
         grid = [[0] * self.COLS for _ in range(self.ROWS)]
         
-        # First pattern
+        # First pattern - A Star is Born
         pattern1 = [
             [0, 0, 1],
             [1, 0, 1],
             [0, 1, 1]
         ]
-        center_row1, center_col1 = self.ROWS // 3, 2* self.COLS // 3
+        center_row1, center_col1 = self.ROWS // 3, 2 * self.COLS // 3
         for i in range(3):
             for j in range(3):
                 grid[center_row1 + i][center_col1 + j] = pattern1[i][j]
 
-        # Second pattern
+        # Second pattern - Celestial Harmony
         pattern2 = [
             [1, 1, 1],
             [1, 0, 1],
@@ -43,10 +43,10 @@ class GameOfLife:
         return grid
 
     def draw_grid(self):
-        self.screen.fill((255, 255, 255))
+        self.screen.fill((0, 0, 0))
         for row in range(self.ROWS):
             for col in range(self.COLS):
-                color = (0, 0, 0) if self.grid[row][col] else (255, 255, 255)
+                color = (255, 255, 255) if self.grid[row][col] else (0, 0, 0)
                 pygame.draw.rect(self.screen, color, (col * self.CELL_SIZE, row * self.CELL_SIZE, self.CELL_SIZE, self.CELL_SIZE), 0)
 
     def update_grid(self):
@@ -63,20 +63,30 @@ class GameOfLife:
         return new_grid
 
     def run_simulation(self):
-        running = True
-        while running:
+        stable_count = 0
+        while stable_count < 10:  # Adjust the stability criterion as needed
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    pygame.quit()
+                    return
 
             self.draw_grid()
             pygame.display.flip()
-            self.grid = self.update_grid()
 
+            new_grid = self.update_grid()
+
+            if self.grid == new_grid:
+                stable_count += 1
+            else:
+                stable_count = 0
+
+            self.grid = new_grid
             self.clock.tick(self.FPS)
 
         pygame.quit()
 
+
 if __name__ == "__main__":
-    game = GameOfLife(1920, 1080, 10, 60)
+    game = GameOfLife(960, 540, 10, 20)
+    #game = GameOfLife(1920, 1080, 20, 20)
     game.run_simulation()
