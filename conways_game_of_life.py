9dae322be80585f7ef4c1beb6acf995562236214
Erik Coleman <erik.coleman@nidec-mce.com>
2023-12-20 17:34:05 -0800

    Update

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -1,5 +1,4 @@
 import pygame
-import numpy as np
 import time
 
 # Define constants
@@ -11,23 +10,18 @@ FPS = 10
 # Initialize pygame
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Conway's Game of Life - Gosper Glider Gun")
+pygame.display.set_caption("Conway's Game of Life - Toroidal Grid")
 
-# Function to initialize the grid with a Gosper glider gun at the center
+# Function to initialize the grid with a glider at the center
 def initialize_grid():
-    grid = np.zeros((ROWS, COLS), dtype=int)
-    gun_pattern = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]
-    center_row, center_col = ROWS // 2 - len(gun_pattern) // 2, COLS // 2 - len(gun_pattern[0]) // 2
-    grid[center_row:center_row + len(gun_pattern), center_col:center_col + len(gun_pattern[0])] = gun_pattern
+    grid = [[0] * COLS for _ in range(ROWS)]
+    glider_pattern = [[0, 1, 0],
+                      [0, 0, 1],
+                      [1, 1, 1]]
+    center_row, center_col = ROWS // 2, COLS // 2
+    for i in range(3):
+        for j in range(3):
+            grid[center_row + i][center_col + j] = glider_pattern[i][j]
     return grid
 
 # Function to draw the grid on the screen
@@ -35,19 +29,21 @@ def draw_grid(grid):
     screen.fill((255, 255, 255))
     for row in range(ROWS):
         for col in range(COLS):
-            color = (0, 0, 0) if grid[row, col] else (255, 255, 255)
+            color = (0, 0, 0) if grid[row][col] else (255, 255, 255)
             pygame.draw.rect(screen, color, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 0)
 
 # Function to update the grid based on Conway's rules with toroidal behavior
 def update_grid(grid):
-    new_grid = np.copy(grid)
+    new_grid = [[0] * COLS for _ in range(ROWS)]
     for row in range(ROWS):
         for col in range(COLS):
-            neighbors = np.sum(grid[(row - 1) % ROWS:(row + 2) % ROWS, (col - 1) % COLS:(col + 2) % COLS]) - grid[row, col]
-            if grid[row, col] == 1 and (neighbors < 2 or neighbors > 3):
-                new_grid[row, col] = 0
-            elif grid[row, col] == 0 and neighbors == 3:
-                new_grid[row, col] = 1
+            neighbors = sum(grid[(row + i) % ROWS][(col + j) % COLS] for i in range(-1, 2) for j in range(-1, 2)) - grid[row][col]
+            if grid[row][col] == 1 and (neighbors < 2 or neighbors > 3):
+                new_grid[row][col] = 0
+            elif grid[row][col] == 0 and neighbors == 3:
+                new_grid[row][col] = 1
+            else:
+                new_grid[row][col] = grid[row][col]
     return new_grid
 
 # Main function to run the simulation
